412665a3784c6fd4333ce1f3f08a57f72efde15b
andreidm92 <admi70@rambler.ru>
2020-01-13 12:24:38 +0300

    Домашнее задание

--- Lesson_2/5.py
+++ Lesson_2/5.py
@@ -3,3 +3,27 @@
 под номером 32 и заканчивая 127-м включительно.
 Вывод выполнить в табличной форме: по десять пар "код-символ" в каждой строке.
 """
+
+for i in range(32, 128):
+    print("{:4d}-{}".format (i, chr(i)), end='')
+    if i % 10 == 0:
+        print()
+
+print()
+print()
+
+'Рекурсия'
+
+def codes(n, m, i):
+    if m-1 > n:
+        i +=1
+        if (i) % 10 == 0:
+            return str("{:4d}-{}".format (n, chr(n))) + '\n' + str(codes(n+1,m, i))
+        else:
+            return str("{:4d}-{}".format(n, chr(n))) + str(codes(n + 1, m, i))
+    else:
+        return str("{:4d}-{}".format(m-1, chr(m-1)))
+
+
+
+print (codes(32,128,1))
