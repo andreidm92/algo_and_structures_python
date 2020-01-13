00e492a8605f3b345511541c7ec9dcab31018a90
andreidm92 <admi70@rambler.ru>
2020-01-13 12:25:37 +0300

    Домашнее задание

--- Lesson_2/9.py
+++ Lesson_2/9.py
@@ -2,3 +2,17 @@
 9. Среди натуральных чисел, которые были введены, найти
 наибольшее по сумме цифр. Вывести на экран это число и сумму его цифр.
 """
+NUM = int(input("Количество цифр? "))
+amax = 0
+SUMMAX = 0
+for i in range(1, NUM + 1):
+    m = int(input("Число " + str(i) + ": "))
+    a = m
+    SUM = 0
+    while m > 0:
+        SUM += m % 10
+        m = m // 10
+    if SUM > SUMMAX:
+          SUMMAX = SUM
+          amax = a
+print (f' Число {amax} c максимальной суммой всех цифр {SUMMAX}')
