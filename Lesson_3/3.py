72823d837ae20f7d5a2098d1ab76bc03453e41a5
andreidm92 <admi70@rambler.ru>
2020-01-16 15:48:57 +0300

    Домашняя работа

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,2 +1,15 @@
-#3.	В массиве случайных целых чисел поменять местами минимальный
+# 3.	В массиве случайных целых чисел поменять местами минимальный
 # и максимальный элементы.
+
+from random import randint
+
+N = int(input('Введите размер массива: '))
+B = []
+for j in range(N):
+    x = randint(-100, 100)
+    B.append(x)
+print(B)
+i = B.index(min(B))
+k = B.index(max(B))
+B[i], B[k] = B[k], B[i]
+print(B)
