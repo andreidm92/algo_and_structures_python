02b8e525637dd55748be9f4be7ecc8b8ae39e778
andreidm92 <admi70@rambler.ru>
2020-01-30 13:26:09 +0300

    Домашнее задание к 7-му уроку

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,20 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+import timeit
+import random
+
+
+def bubble_sort(orig_list):
+    n = 1
+    while n < len(orig_list):
+        for i in range(len(orig_list)-n):
+            if orig_list[-(i + 1)] > orig_list[-(i + 2)]:
+                orig_list[-(i + 1)], orig_list[-(i + 2)] = orig_list[-(i + 2)], orig_list[-(i + 1)]
+        n += 1
+    return orig_list
+
+orig_list = [random.randint(-100, 100) for _ in range(10)]
+print(f'Исходный иассив - {orig_list}')
+print(f'Сортировка по убыванию - {bubble_sort(orig_list)}')
