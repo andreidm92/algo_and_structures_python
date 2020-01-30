82afb1e9e77ab93bc6c35f00353bb302834d2a65
andreidm92 <admi70@rambler.ru>
2020-01-30 13:27:06 +0300

    Домашнее задание к 7-му уроку

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,31 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+import timeit
+import random
+import statistics
+
+def quick_sort(orig_list):
+    if len(orig_list) <= 1:
+        return orig_list
+    else:
+        q = random.choice(orig_list)
+        L = []
+        M = []
+        R = []
+        for elem in orig_list:
+            if elem < q:
+                L.append(elem)
+            elif elem > q:
+                R.append(elem)
+            else:
+                M.append(elem)
+        return quick_sort(L) + M + quick_sort(R)
+
+
+m = int(input('Введите число для расчета размера массива: '))
+orig_list = [random.randint(-100, 100) for _ in range(2*m + 1)]
+sorted_array = quick_sort(orig_list) # Сортируем массив
+print(f' Сгенерированный массив - {orig_list}')
+print(f'Значение Медианы (модуль статистики) - {statistics.median(orig_list)}')
+print(f'Значение Медианы (методом сортировки) - {sorted_array[m]}')
